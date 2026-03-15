c3fbf10bb3d834a008702868cd2d583d49f69162
david <davida.orozco.r@gmail.com>
2026-03-15 09:15:11 -0500

    [FEAT]: Se implementan listings con servicio, controlador y conexión a SQL Server

--- PTruequeU/Program.cs
+++ PTruequeU/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PTruequeU.Data;
 using PTruequeU.Interfaces;
 using PTruequeU.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -12,9 +17,9 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IListingService, ListingService>();
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -22,6 +27,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
