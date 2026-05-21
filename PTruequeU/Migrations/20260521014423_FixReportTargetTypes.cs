f75fe4ebb62bf5cdc0bedb0dd5593891a84da60e
tgarcesm <tomas.garces@eia.edu.co>
2026-05-20 20:45:12 -0500

    fix: corregir TargetType en reportes de usuario del seed + migración FixReportTargetTypes

---
+++ PTruequeU/Migrations/20260521014423_FixReportTargetTypes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PTruequeU.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixReportTargetTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reports",
+                keyColumn: "Report_Id",
+                keyValue: new Guid("11111111-1111-1111-1111-000000000002"),
+                column: "TargetType",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Reports",
+                keyColumn: "Report_Id",
+                keyValue: new Guid("11111111-1111-1111-1111-000000000005"),
+                column: "TargetType",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Reports",
+                keyColumn: "Report_Id",
+                keyValue: new Guid("11111111-1111-1111-1111-000000000009"),
+                column: "TargetType",
+                value: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Reports",
+                keyColumn: "Report_Id",
+                keyValue: new Guid("11111111-1111-1111-1111-000000000002"),
+                column: "TargetType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Reports",
+                keyColumn: "Report_Id",
+                keyValue: new Guid("11111111-1111-1111-1111-000000000005"),
+                column: "TargetType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Reports",
+                keyColumn: "Report_Id",
+                keyValue: new Guid("11111111-1111-1111-1111-000000000009"),
+                column: "TargetType",
+                value: 0);
+        }
+    }
+}
